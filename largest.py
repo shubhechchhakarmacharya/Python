6c38ee4cdeb0a299255e453c0c7dd91e085b9fbf
shubhechchhakarmacharya <80193330+shubhechchhakarmacharya@users.noreply.github.com>
2022-11-06 16:18:33 +0545

    Add files via upload
    beginning of the python.

---
+++ largest.py
@@ -0,0 +1,9 @@
+n1 = int(input("Enter 1st number"))
+n2 = int(input("Enter 2nd number"))
+n3 = int(input("Enter 3rd number"))
+if (n1 > n2 and n1 > n3):
+    print(n1)
+elif(n2>n3):
+    print(n2)
+else:
+    print(n3)
---
+++ random.py
@@ -0,0 +1,5 @@
+import random
+n1 = int(input("Enter starting number."))
+n2 = int(input("Enter ending number"))
+rn = random.randint(n1, n2)
+print(rn)
